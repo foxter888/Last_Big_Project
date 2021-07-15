92bccfb04e377213c81f714ee793f331fb655dab
foxter888 <paul.southworth888@yahoo.com>
2021-07-14 18:16:52 -0700

    added different modlules to make it work

--- src/components/app.js
+++ src/components/app.js
@@ -1,13 +1,15 @@
 import React, { Component } from 'react';
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
+
+import Navigation from "./navigation/nav-links";
 import Home from "./pages/home";
 import About from "./pages/about";
 import Portfolio from "./pages/portfolio";
 import Contact from "./pages/contact";
 import Extra from "./pages/Extra";
 import Auth from "./pages/auth";
-import Navigation from "./navigation/nav-links";
+
 
 
 export default class App extends Component {
@@ -15,11 +17,11 @@ export default class App extends Component {
     return (
       <div className='app'>
         <Router>
-            <Navigation/>
+          <Navigation/>
 
             <Switch>
               <Route exact path="/" component={Home} />
-              <Route exact path="/auth" component={Auth} />
+              <Route path="/auth" component={Auth} />
               <Route path="/about" component={About} />
               <Route path="/portfolio" component={Portfolio} />
               <Route path="/contact" component={Contact} />
