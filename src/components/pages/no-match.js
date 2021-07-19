962c74162ba5ceff175a11f286bb49f51ef77c9e
foxter888 <paul.southworth888@yahoo.com>
2021-07-18 19:27:39 -0700

    addedno match

---
+++ src/components/pages/no-match.js
@@ -0,0 +1,15 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+
+export default function()
+{
+    return(
+        <div>
+           <h2>We couldn't fing that page</h2> 
+            
+            <Link to="/">Return to homepage</Link>
+            
+        </div>
+    );
+}
