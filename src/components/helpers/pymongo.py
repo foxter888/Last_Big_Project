05bdc766ec3c1eb135673b789249515aa51b0252
foxter888 <paul.southworth888@yahoo.com>
2021-07-24 12:55:04 -0700

    added some files in the auth side

--- src/components/helpers/pymongo.py
+++ src/components/helpers/pymongo.py
@@ -2,7 +2,7 @@
 # encoding: utf-8
 import json
 from flask import Flask, request, jsonify
-from flask_mongoengine import MongoEngine
+#from flask_mongoengine import MongoEngine
 
 app = Flask(__name__)
 app.config['MONGODB_SETTINGS'] = {
--- src/components/pages/auth.js
+++ src/components/pages/auth.js
@@ -1,13 +1,110 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 
-class Auth extends Component {
+export default class Auth extends Component {
+    
+    constructor(props)
+    {
+        super(props);
+
+        this.state = {
+            email: "",
+            password: "",
+            errorText: ""
+        }
+
+        this.handleChange = this.handleChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+
+    }
+    
+
+    handleChange(event)
+    {
+        this.setState({
+            [event.target.name]: event.target.value,
+            errorText: ""
+        });
+    }
+    
+
+    handleSubmit(event)
+    {
+        //console.log("handleSubmit", event);
+        axios.post("https://api.devcamp.space/sessions", 
+        {
+            client:
+            {
+                email: this.state.email,
+                password: this.state.password
+            }
+        },
+            {withCredentials: true}
+        ).then(response => {
+            if(response.data.status === "created")
+            {
+                console.log("you can come in");
+                this.props.handleSuccessfulAuth();
+            }
+            else
+            {
+                this.setState({
+                    errorText: "wrong email or password"
+                });
+
+                this.props.handleUnSuccessfulAuth();
+            }
+        }).catch(error => {
+            this.setState({errorText: "an error occured"});
+            this.props.handleUnSuccessfulAuth();
+        });
+
+        event.preventDefault();
+    }
+    
     render() {
         return (
             <div className='auth'>
-                auth
+                <h1>LOGIN TO ACCESS YOUR DASHBOARD</h1>
+               
+               <div>{this.state.errorText}</div>
+
+
+              <form onSubmit={this.handleSubmit} className="auth-form-wrapper">
+                  
+                  
+
+                
+                  <input
+                   type="email"
+                   name="email"
+                   placeholder="your email"
+                   value={this.state.email}
+                   onChange={this.handleChange}
+                  />
+                  
+
+
+                  
+
+               
+                  <input
+                   type="password"
+                   name="password"
+                   placeholder="your password"
+                   value={this.state.password}
+                   onChange={this.handleChange}
+                  />
+                 
+
+               
+               <button type="submit">Login</button>
+               
+
+
+              </form>
             </div>
-        )
+        );
     }
 }
 
-export default Auth;
