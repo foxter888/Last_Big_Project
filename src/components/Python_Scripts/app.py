0aa16e14bbbadfe2bf50eaf48e0f547c43ee599c
foxter888 <paul.southworth888@yahoo.com>
2021-08-04 19:12:41 -0700

    added pythong scripts 2

--- src/components/Python_Scripts/app.py
+++ src/components/Python_Scripts/app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, request, jsonify
 from flask_sqlalchemy import SQLAlchemy
 from flask_marshmallow import Marshmallow
 import os
@@ -11,9 +11,9 @@ ma = Marshmallow(app)
 
 
 class Guide(db.Model):
-    id = db.column(db.Integer, primary_key= True)
-    title = db.column(db.String(100), unique = False)
-    content = db.column(db.String(144), unique = False)
+    id = db.Column(db.Integer, primary_key= True)
+    title = db.Column(db.String(100), unique = False)
+    content = db.Column(db.String(144), unique = False)
 
     def __init__(self, title, content):
         self.title = title
@@ -29,6 +29,29 @@ class GuideSchema(ma.Schema):
 guide_schema = GuideSchema()
 guides_schema = GuideSchema(many=True)
 
+# end point to create a new guide 
+@app.route('/guide', methods= ["POST"])
+def add_guide():
+    title = request.json['title']
+    content = request.json['content']
+
+
+    new_guide = Guide(title, content)
+
+    db.session.add(new_guide)
+    db.session.commit()
+
+    guide = Guide.query.get(new_guide.id)
+
+    return guide_schema.jsonify(guide)
+
+
+# Endpoint to query all guides
+@app.route("/guides", methods=["GET"])
+def get_guides():
+    all_guides = Guide.query.all()
+    result = guides_schema.dump(all_guides)
+    return jsonify(result)
 
 
 @app.route('/')
