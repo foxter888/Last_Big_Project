3e89a79d48b87aeac32ae593e160ca608db76d02
foxter888 <paul.southworth888@yahoo.com>
2021-08-25 19:33:54 -0700

    added sign up

--- src/components/pages/signup.js
+++ src/components/pages/signup.js
@@ -1,13 +1,32 @@
 import React, { Component } from 'react';
 
+import SignupForm from './signupForm';
+
+
+import { connect } from 'react-redux';
+//import * as actions from '../../actions';
+
 class Signup extends Component {
+
+    componentDidMount() {
+       // this.props.setHeaderLinks([]);
+        //this.props.setNavbarLinks([]);
+    }
+
+    onSubmit = (fields) => {
+        console.log(fields);
+    }
+
     render() {
         return (
-            <div className="signup">
-                Sign up page
+            <div className='sign-up'>
+                
+                <SignupForm onSubmit={this.onSubmit} className='sign-up__form' />
             </div>
         )
     }
 }
+
+//Signup = connect(null, actions)(SignUp);
 
 export default Signup;
